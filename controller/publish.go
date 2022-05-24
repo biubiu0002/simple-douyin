851e9667747ff9e27e547e3ecf865c7c479dfe1f
叶锐坚 <i_yeruijian@cvte.com>
2022-05-24 20:01:38 +0800

    [modify]新增.gitignore文件 新增一条注释test

--- controller/publish.go
+++ controller/publish.go
@@ -49,6 +49,7 @@ func Publish(c *gin.Context) {
 
 // PublishList all users have same publish video list
 func PublishList(c *gin.Context) {
+	//开始学go 哈哈
 	c.JSON(http.StatusOK, VideoListResponse{
 		Response: Response{
 			StatusCode: 0,
